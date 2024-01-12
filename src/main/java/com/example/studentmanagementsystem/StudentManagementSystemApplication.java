1dbbebddb9dcd56bf3fcbc859833bec7759956b9
Najrul Islam <najrulbiplob@hotmail.com>
2024-01-12 14:07:46 -0500

    Adding project to GitHub repo

---
+++ src/main/java/com/example/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -0,0 +1,34 @@
+package com.example.studentmanagementsystem;
+
+import com.example.studentmanagementsystem.entity.Student;
+import com.example.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+/*	@Override
+	public void run(String... args) throws Exception {
+*//*		Student student1 = new Student("Najrul", "Islam", "nislam611@cable.comcast.com");
+		studentRepository.save(student1);
+
+		Student student2 = new Student("Syful", "Islam", "syfulislam42@hotmail.com");
+		studentRepository.save(student2);
+
+		Student student3 = new Student("Saifan", "Islam", "Saifan_islam@gmail.com");
+		studentRepository.save(student3);*//*
+
+	}*/
+
+	//You are at 55:58 (about to write the footer)
+}
---
+++ src/main/java/com/example/studentmanagementsystem/impl/StudentServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.studentmanagementsystem.impl;
+
+import com.example.studentmanagementsystem.entity.Student;
+import com.example.studentmanagementsystem.repository.StudentRepository;
+import com.example.studentmanagementsystem.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        Student student = getStudentById(id);
+        studentRepository.delete(student);
+    }
+}
